e998bdae0ebd39a771acea2440122b7e70c65c9a
Jatupol Nampromma <beerzaonline@hotmail.com>
2017-02-06 13:01:26 +0700

    new

--- src/main/java/rmuti/arraylist/App.java
+++ src/main/java/rmuti/arraylist/App.java
@@ -1,14 +1,18 @@
 package rmuti.arraylist;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	ArrayList list = new ArrayList();
-    	list.add(1,"TEST");
-    	
-    	
-    	
-       
-    }
+public class App {
+	public static void main(String[] args) {
+		ArrayList arrlist = new ArrayList();
+		arrlist.add(20);
+		arrlist.add(30);
+		arrlist.add(40);
+		arrlist.add(50);
+		arrlist.add(2, 100);
+		arrlist.addFirst(70);
+		arrlist.addLast(90);
+		// arrlist.remove(1);
+		//arrlist.removeEven();
+		System.out.println(arrlist);
+
+	}
 }
--- src/main/java/rmuti/arraylist/ArrayList.java
+++ src/main/java/rmuti/arraylist/ArrayList.java
@@ -1,5 +1,7 @@
 package rmuti.arraylist;
 
+import java.util.Arrays;
+
 public class ArrayList {
 	private Object[] elementData = new Object[1];
 	private int size = 0;
@@ -11,21 +13,58 @@ public class ArrayList {
 		}
 		elementData[i] = e;
 		size++;
-
 	}
 
 	public void add(Object e) {
-		// elementData[size++]=e;
-		add(size++, e);
+		ensureCapacity(size + 1);
+		elementData[size++] = e;
 	}
 
-	public void ensureCapacity(int capacity) {
-		int x = 2 * elementData.length;
-		Object[] arr = new Object[x];
-		for (int i = 0; i < size; i++) {
-			arr[i] = elementData[i];
+	public void addFirst(int n) {
+		elementData[0] = n;
+	}
+
+	public void addLast(int n) {
+		elementData[size - 1] = n;
+	}
+
+	public void remove(int index) {
+		for (int j = index; j < size - 1; j++) {
+			elementData[j] = elementData[j + 1];
 		}
-		elementData = arr;
+		elementData[size - 1] = null;
+	}
+
+	// public void removeEven() {
+	// for (int i = 0; i < size - 1; i++) {
+	//
+	// if (check % 2 == 0) {
+	// for (int j = i; j < size - 1; j++) {
+	// elementData[j] = elementData[j + 1];
+	// }
+	// elementData[size - 1] = null;
+	// }
+	// }
+	// }
+
+	public void ensureCapacity(int minCapacity) {
+		int oldCapacity = elementData.length;
+		if (minCapacity > oldCapacity) {
+			Object oldData[] = elementData;
+			int newCapacity = (oldCapacity * 3) / 2 + 1;
+			if (newCapacity < minCapacity)
+				newCapacity = minCapacity;
+			Object[] arr = new Object[newCapacity];
+			for (int i = 0; i < size; i++) {
+				arr[i] = elementData[i];
+			}
+			elementData = arr;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(elementData);
 	}
 
 }
